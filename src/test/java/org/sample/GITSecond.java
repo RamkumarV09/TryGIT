3a215cf1103ea95f65194a2495fe22e37d21f0cf
RamkumarV09 <ramkumarv0986@gmail.com>
2024-09-16 22:17:33 +0530

    Second GIT Code

---
+++ src/test/java/org/sample/GITSecond.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class GITSecond {
+	private void meth1() {
+		
+		System.out.println("Meth1");
+
+	}
+	
+	private void meth2() {
+		System.out.println("Method 2");
+
+	}
+	private void meth3() {
+		System.out.println("Method 3");
+
+	}
+	
+	
+
+}
